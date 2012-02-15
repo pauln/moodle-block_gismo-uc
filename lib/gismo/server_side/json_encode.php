865dc5b9fe100d95de1640d3926b8024c91d80f4
Paul Nicholls <paul.nicholls@canterbury.ac.nz>
2012-02-15 15:49:46 +1300

    blocks/gismo: use a local variable for the Services_JSON object instance instead of a global one

--- lib/gismo/server_side/json_encode.php
+++ lib/gismo/server_side/json_encode.php
@@ -1,9 +1,8 @@
 <?php
 require_once('JSON.php');
-$GISMO_JSON = new Services_JSON();
 function json_encode($arr) {
-    global $GISMO_JSON;
-    return $GISMO_JSON->encode($arr);
+    $json = new Services_JSON();
+    return $json->encode($arr);
 }
 
 ?>
